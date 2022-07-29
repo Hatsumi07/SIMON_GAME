62742cbfe6a0d6ba50eb306fcf574096690c0dc0
Hatsumi07 <64712742+Hatsumi07@users.noreply.github.com>
2022-07-28 19:32:11 -0500

    Add files via upload

---
+++ simon/game/directing/scene_manager.py
@@ -0,0 +1,230 @@
+import csv
+from constants import *
+from game.casting.body import Body
+from game.casting.pad import Pad
+from game.casting.beat_sequence import BeatSequence
+from game.casting.pads_to_play import PadsToPlay
+from game.casting.image import Image
+from game.casting.label import Label
+from game.casting.point import Point
+from game.casting.stats import Stats
+from game.casting.text import Text 
+from game.scripting.change_scene_action import ChangeSceneAction
+from game.scripting.check_over_action import CheckOverAction
+from game.scripting.control_actors_action import ControlActorsAction
+from game.scripting.draw_dialog_action import DrawDialogAction
+from game.scripting.draw_hud_action import DrawHudAction
+from game.scripting.draw_actors_action import DrawActorsAction
+from game.scripting.end_drawing_action import EndDrawingAction
+from game.scripting.initialize_devices_action import InitializeDevicesAction
+from game.scripting.load_assets_action import LoadAssetsAction
+from game.scripting.play_sound_action import PlaySoundAction
+from game.scripting.release_devices_action import ReleaseDevicesAction
+from game.scripting.start_drawing_action import StartDrawingAction
+from game.scripting.timed_change_scene_action import TimedChangeSceneAction
+from game.scripting.unload_assets_action import UnloadAssetsAction
+from game.scripting.play_pad_action import PlayPadAction
+from game.services.raylib.raylib_audio_service import RaylibAudioService
+from game.services.raylib.raylib_keyboard_service import RaylibKeyboardService
+from game.services.raylib.raylib_physics_service import RaylibPhysicsService
+from game.services.raylib.raylib_video_service import RaylibVideoService
+from game.services.raylib.raylib_mouse_service import RaylibMouseService
+
+class SceneManager:
+    """The person in charge of setting up the cast and script for each scene."""
+    
+    AUDIO_SERVICE = RaylibAudioService()
+    KEYBOARD_SERVICE = RaylibKeyboardService()
+    MOUSE_SERVICE = RaylibMouseService()
+    PHYSICS_SERVICE = RaylibPhysicsService()
+    VIDEO_SERVICE = RaylibVideoService(GAME_NAME, SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    CHECK_OVER_ACTION = CheckOverAction()
+    CONTROL_ACTORS_ACTION = ControlActorsAction(MOUSE_SERVICE, AUDIO_SERVICE, VIDEO_SERVICE)
+    DRAW_DIALOG_ACTION = DrawDialogAction(VIDEO_SERVICE)
+    DRAW_HUD_ACTION = DrawHudAction(VIDEO_SERVICE)
+    DRAW_ACTORS_ACTION = DrawActorsAction(VIDEO_SERVICE)
+    END_DRAWING_ACTION = EndDrawingAction(VIDEO_SERVICE)
+    INITIALIZE_DEVICES_ACTION = InitializeDevicesAction(AUDIO_SERVICE, VIDEO_SERVICE)
+    LOAD_ASSETS_ACTION = LoadAssetsAction(AUDIO_SERVICE, VIDEO_SERVICE)
+    RELEASE_DEVICES_ACTION = ReleaseDevicesAction(AUDIO_SERVICE, VIDEO_SERVICE)
+    START_DRAWING_ACTION = StartDrawingAction(VIDEO_SERVICE)
+    UNLOAD_ASSETS_ACTION = UnloadAssetsAction(AUDIO_SERVICE, VIDEO_SERVICE)
+
+
+    def __init__(self):
+        pass
+
+    def prepare_scene(self, scene, cast, script):
+        if scene == NEW_GAME:
+            self._prepare_new_game(cast, script)
+        elif scene == NEXT_LEVEL:
+            self._prepare_next_level(cast, script)
+        elif scene == TRY_AGAIN:
+            self._prepare_try_again(cast, script)
+        elif scene == IN_PLAY:
+            self._play_pads(cast, script)
+        elif scene == PLAYER_TURN:
+            self._player_turn(cast, script)
+        elif scene == GAME_OVER:    
+            self._prepare_game_over(cast, script)
+    
+    # ----------------------------------------------------------------------------------------------
+    # scene methods
+    # ----------------------------------------------------------------------------------------------
+    
+    def _prepare_new_game(self, cast, script):
+        print("                 PREPARE NEW GAME                           ")
+        self._add_stats(cast)
+        self._add_level(cast)
+        self._add_lives(cast)
+        self._add_score(cast)
+        self._add_pads(cast)
+        self._add_pads_to_play(cast)
+        self._add_dialog(cast, ENTER_TO_START)
+
+        self._add_initialize_script(script)
+        self._add_load_script(script)
+        script.clear_actions(INPUT)
+        script.add_action(INPUT, ChangeSceneAction(True, self.KEYBOARD_SERVICE, NEXT_LEVEL))
+        self._add_output_script(script)
+        self._add_unload_script(script)
+        self._add_release_script(script)
+
+
+    def _prepare_next_level(self, cast, script):
+        print("                 PREPARE NEXT LEVEL                           ")
+        self._add_pads(cast)
+        self._add_dialog(cast, PREP_TO_LAUNCH)
+        
+        script.clear_actions(INPUT)
+        script.add_action(INPUT, TimedChangeSceneAction(IN_PLAY, 2))
+        self._add_output_script(script)
+        script.add_action(OUTPUT, PlaySoundAction(self.AUDIO_SERVICE, WELCOME_SOUND))
+
+    def _prepare_try_again(self, cast, script):
+        print("                 PREPARE TRY AGAIN                           ")
+        self._add_pads(cast)
+        self._add_dialog(cast, PREP_TO_LAUNCH)
+
+        script.clear_actions(INPUT)
+        script.add_action(INPUT, TimedChangeSceneAction(IN_PLAY, 2))
+        self._add_update_script(cast, script)
+        self._add_output_script(script)
+
+    def _play_pads(self, cast, script):
+        print("                 PREPARE PLAY PADS                           ")
+        print("AUTOPLAY MODE")
+        cast.clear_actors(DIALOG_GROUP)
+        script.clear_actions(INPUT)
+        script.clear_actions(CHANGE)
+        #script.add_action(CHANGE, ChangeSceneAction(False, self.KEYBOARD_SERVICE, PLAYER_TURN))
+        self._add_update_script(cast, script)
+        self._add_output_script(script)
+
+    def _player_turn(self, cast, script):
+        print("                 PREPARE PLAYER TURN                           ")
+        print("PLAYER TURN")
+        script.clear_actions(INPUT)
+        script.clear_actions(UPDATE)
+        script.clear_actions(CHANGE)
+
+        self._add_pads_to_play(cast)
+        #script.add_action(CHANGE, ChangeSceneAction(False, self.KEYBOARD_SERVICE, IN_PLAY))
+        script.add_action(INPUT, self.CONTROL_ACTORS_ACTION)
+        self._add_output_script(script)
+
+    def _prepare_game_over(self, cast, script):
+        print("                 PREPARE GAME OVER                           ")
+        self._add_pads(cast)
+        self._add_dialog(cast, WAS_GOOD_GAME)
+
+        script.clear_actions(INPUT)
+        script.add_action(INPUT, TimedChangeSceneAction(NEW_GAME, 5))
+        script.clear_actions(UPDATE)
+        self._add_output_script(script)
+
+    # ----------------------------------------------------------------------------------------------
+    # casting methods
+    # ----------------------------------------------------------------------------------------------
+
+
+    def _add_dialog(self, cast, message):
+        cast.clear_actors(DIALOG_GROUP)
+        text = Text(message, FONT_FILE, FONT_SMALL, ALIGN_CENTER)
+        position = Point(CENTER_X, CENTER_Y)
+        label = Label(text, position)
+        cast.add_actor(DIALOG_GROUP, label)
+
+    def _add_level(self, cast):
+        cast.clear_actors(LEVEL_GROUP)
+        text = Text(LEVEL_FORMAT, FONT_FILE, FONT_SMALL, ALIGN_LEFT)
+        position = Point(HUD_MARGIN, HUD_MARGIN)
+        label = Label(text, position)
+        cast.add_actor(LEVEL_GROUP, label)
+
+    def _add_lives(self, cast):
+        cast.clear_actors(LIVES_GROUP)
+        text = Text(LIVES_FORMAT, FONT_FILE, FONT_SMALL, ALIGN_RIGHT)
+        position = Point(SCREEN_WIDTH - HUD_MARGIN, HUD_MARGIN)
+        label = Label(text, position)
+        cast.add_actor(LIVES_GROUP, label)
+
+    def _add_score(self, cast):
+        cast.clear_actors(SCORE_GROUP)
+        text = Text(SCORE_FORMAT, FONT_FILE, FONT_SMALL, ALIGN_CENTER)
+        position = Point(CENTER_X, HUD_MARGIN)
+        label = Label(text, position)
+        cast.add_actor(SCORE_GROUP, label)
+
+    def _add_stats(self, cast):
+        cast.clear_actors(STATS_GROUP)
+        stats = Stats()
+        cast.add_actor(STATS_GROUP, stats)
+    
+    def _add_pads(self, cast):
+        pad_group = ["pad1", "pad2", "pad3", "pad4"]
+        for pad in pad_group:
+            cast.clear_actors(pad)
+        cast.add_actor("pad1", Pad("pad1", RED_PAD_POSITION, PAD_SIZE, RED, RED_PAD_SOUND))
+        cast.add_actor("pad2", Pad("pad2", BLUE_PAD_POSITION, PAD_SIZE, BLUE, BLUE_PAD_SOUND))
+        cast.add_actor("pad3", Pad("pad3", GREEN_PAD_POSITION, PAD_SIZE, GREEN, GREEN_PAD_SOUND))
+        cast.add_actor("pad4", Pad("pad4", YELLOW_PAD_POSITION, PAD_SIZE, YELLOW, YELLOW_PAD_SOUND))
+
+    def _add_sequence(self, cast):
+        pads = [cast.get_first_actor("pad1"), cast.get_first_actor("pad2"), cast.get_first_actor("pad3"), cast.get_first_actor("pad4")]
+        cast.clear_actors("beat_sequence")
+        cast.add_actor("beat_sequence", BeatSequence(pads))
+    
+    def _add_pads_to_play(self, cast):
+        cast.add_actor("pads_to_play", PadsToPlay())
+    # ----------------------------------------------------------------------------------------------
+    # scripting methods
+    # ----------------------------------------------------------------------------------------------
+    def _add_initialize_script(self, script):
+        script.clear_actions(INITIALIZE)
+        script.add_action(INITIALIZE, self.INITIALIZE_DEVICES_ACTION)
+
+    def _add_load_script(self, script):
+        script.clear_actions(LOAD)
+        script.add_action(LOAD, self.LOAD_ASSETS_ACTION)
+    
+    def _add_output_script(self, script):
+        script.clear_actions(OUTPUT)
+        script.add_action(OUTPUT, self.START_DRAWING_ACTION)
+        script.add_action(OUTPUT, self.DRAW_HUD_ACTION)
+        script.add_action(OUTPUT, self.DRAW_ACTORS_ACTION)
+        script.add_action(OUTPUT, self.DRAW_DIALOG_ACTION)
+        script.add_action(OUTPUT, self.END_DRAWING_ACTION)
+
+    def _add_release_script(self, script):
+        script.clear_actions(RELEASE)
+        script.add_action(RELEASE, self.RELEASE_DEVICES_ACTION)
+    
+    def _add_unload_script(self, script):
+        script.clear_actions(UNLOAD)
+        script.add_action(UNLOAD, self.UNLOAD_ASSETS_ACTION)
+
+    def _add_update_script(self, cast, script):
+        script.clear_actions(UPDATE)
+        script.add_action(UPDATE, PlayPadAction(self.AUDIO_SERVICE))
